7889a3f8ad86e418354c50cd3b05ad67627b2aa2
Adriano de Azevedo <contato@adriano.dev>
2022-11-07 11:02:21 -0300

    exercicio 19

--- exercicios/exercicio-19/resposta/pages/posts/first-post.tsx
+++ exercicios/exercicio-19/resposta/pages/posts/first-post.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
--- exercicios/exercicio-19/resposta/pages/profile.tsx
+++ exercicios/exercicio-19/resposta/pages/profile.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
 export default function Profile() {
   return (
     <>
+      <Head>
+        <title>Profile</title>
+      </Head>
       <h1>Adriano de Azevedo</h1>
-      <img src="/images/profile.jpg" alt="Adriano de Azevedo" />
+      <Image src="/images/profile.jpg" alt="Adriano de Azevedo" width={144} height={144} />
     </>
   );
 }
